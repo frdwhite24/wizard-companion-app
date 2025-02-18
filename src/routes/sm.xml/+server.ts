453ff815ce5b83bdb4cf2accc0fb121ba5684f9f
Fred White <fredwhite.dev@gmail.com>
2025-02-18 08:51:36 +0100

    fix(sitemap): match the sitemaps exactly so we can isolate the issue

--- src/routes/sm.xml/+server.ts
+++ src/routes/sm.xml/+server.ts
@@ -7,15 +7,11 @@ export const GET: RequestHandler = async () => {
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
         <loc>https://d3cfpk4ju180uf.cloudfront.net</loc>
-        <changefreq>daily</changefreq>
+        <lastmod>2025-02-18</lastmod>
+        <changefreq>weekly</changefreq>
         <priority>1.0</priority>
       </url>
     </urlset>`.trim()
 
-  return new Response(sitemap, {
-    headers: {
-      'Content-Type': 'application/xml; charset=utf-8',
-      'Cache-Control': 'max-age=0, s-maxage=3600',
-    },
-  })
+  return new Response(sitemap)
 }
