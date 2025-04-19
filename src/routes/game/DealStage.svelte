32050cfb36d8b7774211cc0f5c4461b787579090
Fred White <fredwhite.dev@gmail.com>
2025-04-19 10:54:50 +0200

    feat(trump help): some help text to provide guidance when trump is turned over
    this was always a problem and should really help us when we play
    
    fix #3

--- src/routes/game/DealStage.svelte
+++ src/routes/game/DealStage.svelte
@@ -9,9 +9,8 @@
 <h1>Deal the cards</h1>
 <p>
   <strong>{dealer}</strong> deals <strong>{currentRound}</strong>
-  {currentRound === 1 ? 'card' : 'cards'} to each player
+  {currentRound === 1 ? 'card' : 'cards'} to each player, and turns over the trump
 </p>
-
 <div class="players">
   {#each players as player, index}
     <div class="player-row">
@@ -22,13 +21,22 @@
     </div>
   {/each}
 </div>
+<details>
+  <summary class="summary-text">What does the trump mean?</summary>
+  <p>
+    If there are no cards left or a jester is turned there is no trump suit,
+    only the wizards are trump.
+  </p>
+  <br />
+  <p>If a wizard is turned the dealer picks a trump suit.</p>
+</details>
 
 <style>
   .players {
     display: flex;
     flex-direction: column;
     gap: 24px;
-    margin-top: 2rem;
+    margin: 2rem 0;
   }
 
   .player-row {
@@ -43,5 +51,20 @@
 
   small {
     color: var(--pico-secondary);
+  }
+
+  details {
+    border: 1px solid var(--pico-primary-border);
+    padding: 16px;
+    border-radius: var(--pico-border-radius);
+  }
+
+  details p {
+    margin-bottom: 0;
+  }
+
+  details[open] > summary:not([role]):not(:focus),
+  details summary {
+    color: var(--pico-accordion-active-summary-color);
   }
 </style>
