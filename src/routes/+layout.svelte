162ae971b4fcb9102d389d5e12bd36eba16eb75d
Fred White <fredwhite.dev@gmail.com>
2025-06-01 17:17:58 +0200

    feat(posthog): add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,17 @@
   import { onMount } from 'svelte'
   import { registerServiceWorker, updateThemeColor } from '$lib/pwa'
   import '../app.css'
+  import { browser } from '$app/environment'
+  import posthog from 'posthog-js'
 
   onMount(() => {
+    if (browser) {
+      posthog.init('phc_9e5NLbGFPNnQ5eUWMheIPXin3agu1ZrSuAGJWnho0Mn', {
+        api_host: 'https://eu.i.posthog.com',
+        person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+      })
+    }
+
     registerServiceWorker()
     updateThemeColor()
 
